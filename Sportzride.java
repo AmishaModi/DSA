c23e9e00284f9e26178fe48c5048e8baf1d6a5a6
BreejeModi <73386585+BreejeModi@users.noreply.github.com>
2020-10-24 16:11:10 +0530

    Add files via upload

---
+++ Sportzride.java
@@ -0,0 +1,14 @@
+package com.JavaPractice;
+
+
+public class Sportzride {
+    private int runs;
+    void liveMatch(){
+      System.out.println("Live Match Updates");
+    }
+    void runs(){
+        runs = 50;
+        System.out.println(runs);
+    }
+
+}
