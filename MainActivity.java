142e6e098e26eb2ae2bf6ef588bcb6edf342d667
AmishaModi <73391838+AmishaModi@users.noreply.github.com>
2020-10-24 18:45:05 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,117 +1,62 @@
-package com.breejemodi.cricket;
+package com.breejemodi.instaclone;
 
-import android.app.DownloadManager;
-import android.app.ProgressDialog;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.widget.ProgressBar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+import com.parse.ParseException;
+import com.parse.ParseInstallation;
+import com.parse.ParseObject;
+import com.parse.SaveCallback;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.TimeZone;
-
-public class MainActivity extends AppCompatActivity {
-
-    private RecyclerView mRecyclerView;
-
-    private String url = "https://cricapi.com/api/matches?apikey=msi87gPqdrf2q0dt29B4VO2Es0z1";
-
-    private RecyclerView.Adapter mAdapter;
-    private List<Model> modelList;
+    private Button btnSave;
+    private EditText edtName, edtEmail, edtNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mRecyclerView = findViewById(R.id.recyclerview);
+        ParseInstallation.getCurrentInstallation().saveInBackground();
 
-        mRecyclerView.setHasFixedSize(true);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        btnSave = findViewById(R.id.btnSave);
 
-        modelList = new ArrayList<>();
+        edtName = findViewById(R.id.edtName);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtNumber = findViewById(R.id.edtNumber);
 
-        loadUrlData();
+        btnSave.setOnClickListener(this);
+
     }
 
-    private void loadUrlData() {
 
-        ProgressDialog pd = new ProgressDialog(this);
-        pd.setMessage("Loading...");
-        pd.show();
 
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+    @Override
+    public void onClick(View view) {
+
+        final ParseObject userData = new ParseObject("User Data");
+
+        userData.put("Name", edtName.getText().toString());
+        userData.put("Email", edtEmail.getText().toString());
+        userData.put("Mobile_Number", edtNumber.getText().toString());
+
+        userData.saveInBackground(new SaveCallback() {
             @Override
-            public void onResponse(String response) {
-
-                try{
-
-                    JSONArray jsonArray = new JSONObject(response).getJSONArray("matches");
-
-                    for(int i=0; i<jsonArray.length(); i++){
-
-                        try{
-                            String uniqueId = jsonArray.getJSONObject(i).getString("unique_id");
-                            String team1 = jsonArray.getJSONObject(i).getString("team-1");
-                            String team2 = jsonArray.getJSONObject(i).getString("team-2");
-                            String matchType = jsonArray.getJSONObject(i).getString("type");
-                            String matchStatus = jsonArray.getJSONObject(i).getString("matchStarted");
-                            if(matchStatus.equals("true")){
-                                matchStatus = "Match Started";
-                            }
-                            else{
-                                matchStatus = "Match Not Started";
-                            }
-
-                            String dateTimeGMT = jsonArray.getJSONObject(i).getString("dateTimeGMT");
-
-                            SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
-                            format1.setTimeZone(TimeZone.getTimeZone(dateTimeGMT));
-                            Date date = format1.parse(dateTimeGMT);
-
-                            SimpleDateFormat format2 = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-                            format2.setTimeZone(TimeZone.getTimeZone("GMT"));
-
-                            String dateTime = format2.format(date);
-
-                            Model model = new Model(uniqueId,team1,team2,matchType,matchStatus,dateTime);
-                            modelList.add(model);
-                        }
-                        catch (Exception e){
-                            Toast.makeText(MainActivity.this, "Error " + e.getMessage(), Toast.LENGTH_LONG).show();
-                        }
-                    }
-
-                    mAdapter = new MyAdapter(modelList, getApplicationContext());
-                    mRecyclerView.setAdapter(mAdapter);
-
-                } catch (Exception e){
-                    Toast.makeText(MainActivity.this, "Error " + e.getMessage(), Toast.LENGTH_LONG).show();
-                };
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(MainActivity.this, "Error " + error.toString(), Toast.LENGTH_LONG).show();
+            public void done(ParseException e) {
+                if (e == null) {
+                    Toast.makeText(MainActivity.this, edtName.getText().toString() + ", Your Info is Saved", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                }
             }
         });
-
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(stringRequest);
     }
 }
+
+
