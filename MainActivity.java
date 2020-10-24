291f5f0dfdb902e36ad1b703421776c6206781ae
AmishaModi <73391838+AmishaModi@users.noreply.github.com>
2020-10-24 18:53:34 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,62 +1,245 @@
-package com.breejemodi.instaclone;
+package com.mystartup.fingerspeedgame;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.parse.ParseException;
-import com.parse.ParseInstallation;
-import com.parse.ParseObject;
-import com.parse.SaveCallback;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView timerTextView;
+    private TextView aThousandTextView;
+    private Button tapTapButton;
 
-    private Button btnSave;
-    private EditText edtName, edtEmail, edtNumber;
+    private CountDownTimer countDownTimer;
+
+    private long initialCountDownInMillis = 60000;
+    private int timerInterval = 1000;
+    private int remainingTime = 60;
+
+    private int aThousand = 1000;
+
+    private final String REMAINING_TIME_KEY = "remaining time key";
+    private final String A_THOUSAND_KEY = "a thousand key";
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        showToast("On Destroy Method is Called", Toast.LENGTH_SHORT);
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        showToast("On Save Instance Is Called", Toast.LENGTH_SHORT);
+
+        outState.putInt(REMAINING_TIME_KEY, remainingTime);
+        outState.putInt(A_THOUSAND_KEY, aThousand);
+        countDownTimer.cancel();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ParseInstallation.getCurrentInstallation().saveInBackground();
 
-        btnSave = findViewById(R.id.btnSave);
+        showToast("On Create Methods is Called", Toast.LENGTH_SHORT);
 
-        edtName = findViewById(R.id.edtName);
-        edtEmail = findViewById(R.id.edtEmail);
-        edtNumber = findViewById(R.id.edtNumber);
+        timerTextView = findViewById(R.id.txtTimer);
+        aThousandTextView = findViewById(R.id.txtAThousand);
+        tapTapButton = findViewById(R.id.btnTap);
 
-        btnSave.setOnClickListener(this);
+        aThousandTextView.setText(aThousand + "");
 
-    }
+        if (savedInstanceState != null) {
 
+            remainingTime = savedInstanceState.getInt(REMAINING_TIME_KEY);
+            aThousand = savedInstanceState.getInt(A_THOUSAND_KEY);
 
+            restoreTheGame();
 
-    @Override
-    public void onClick(View view) {
+        }
 
-        final ParseObject userData = new ParseObject("User Data");
-
-        userData.put("Name", edtName.getText().toString());
-        userData.put("Email", edtEmail.getText().toString());
-        userData.put("Mobile_Number", edtNumber.getText().toString());
-
-        userData.saveInBackground(new SaveCallback() {
+        tapTapButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void done(ParseException e) {
-                if (e == null) {
-                    Toast.makeText(MainActivity.this, edtName.getText().toString() + ", Your Info is Saved", Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+            public void onClick(View view) {
+
+                aThousand--;
+                //aThousand = aThousand - 1;
+
+                aThousandTextView.setText(aThousand + "");
+
+                if (remainingTime > 0 && aThousand <= 0) {
+
+                    Toast.makeText(MainActivity.this, "Congratulation", Toast.LENGTH_SHORT).show();
+
+                    showAlert("Congratulations", "Please reset the Game");
+
                 }
             }
         });
+
+
+        if (savedInstanceState == null) {
+            countDownTimer = new CountDownTimer(initialCountDownInMillis, timerInterval) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+
+                    remainingTime = (int) millisUntilFinished / 1000;
+                    timerTextView.setText(remainingTime + "");
+
+                }
+
+                @Override
+                public void onFinish() {
+
+                    Toast.makeText(MainActivity.this, "Countdown finished", Toast.LENGTH_SHORT).show();
+
+
+                    showAlert("Not interesting", "Would you like to try again?");
+
+
+                }
+            };
+
+
+            countDownTimer.start();
+        }
+
+    }
+
+    private void restoreTheGame() {
+
+        int restoredRemainingTime = remainingTime;
+        int restoredAThousand = aThousand;
+
+        timerTextView.setText(restoredRemainingTime + "");
+        aThousandTextView.setText(restoredAThousand + "");
+
+        countDownTimer = new CountDownTimer((long)remainingTime * 1000, timerInterval) {
+            @Override
+            public void onTick(long milisUntilFinished) {
+
+                remainingTime = (int) milisUntilFinished / 1000;
+                timerTextView.setText(remainingTime + "");
+
+
+
+            }
+
+            @Override
+            public void onFinish() {
+
+                showAlert("Finished", "Would you like to reset the game?");
+
+            }
+        };
+
+
+        countDownTimer.start();
+
+    }
+
+    private void resetTheGame() {
+
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+
+            countDownTimer = null;
+        }
+        aThousand = 1000;
+        aThousandTextView.setText(Integer.toString(aThousand));
+
+        timerTextView.setText(remainingTime + "");
+
+        countDownTimer = new CountDownTimer(initialCountDownInMillis, timerInterval) {
+            @Override
+            public void onTick(long millisToFinish) {
+
+                remainingTime = (int) millisToFinish / 1000;
+                timerTextView.setText(remainingTime + "");
+
+            }
+
+            @Override
+            public void onFinish() {
+
+
+                showAlert("Finished", "Would you like to reset the game?");
+
+            }
+        };
+
+
+
+        countDownTimer.start();
+
+    }
+
+    private void showAlert(String title, String message) {
+
+
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //set what would happen when positive button is clicked
+
+                        resetTheGame();
+
+                    }
+                }).show();
+        alertDialog.setCancelable(false);
+
+
+
+    }
+
+    private void showToast(String message, int duration) {
+
+        Toast.makeText(this, message, duration).show();
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.info_item) {
+
+
+        showToast("This is the Current Version of your Game. Check out Google Play and Make sure that you're playing the latest version of the gamne" + BuildConfig.VERSION_NAME, Toast.LENGTH_LONG);
+
+        }
+
+        return true;
+
     }
 }
-
-
